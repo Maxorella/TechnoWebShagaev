b8ac7b9a66f4acf3760fb00d92903d347c673465
Maksim Shagaev <maxorella@mail.ru>
2023-12-27 16:18:58 +0300

    переименовал папки

--- askme_Shagaev/asgi.py
+++ askme_Shagaev/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for TechnoWebShagaev project.
+ASGI config for askme_Shagaev project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'TechnoWebShagaev.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'askme_Shagaev.settings')
 
 application = get_asgi_application()
--- askme_Shagaev/urls.py
+++ askme_Shagaev/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for TechnoWebShagaev project.
+URL configuration for askme_Shagaev project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
